2bbd4a3a12768124d44967027b92a67711680d76
REVENANT <85705268+rakeshsarkar9711@users.noreply.github.com>
2023-11-30 10:45:02 +0530

    Add files via upload

--- piai.py
+++ piai.py
@@ -5,6 +5,43 @@ from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pathlib
+import warnings
+import pyttsx3
+import speech_recognition as sr
+
+from Clap import MainClapExe
+MainClapExe()
+
+warnings.simplefilter('ignore')
+
+# nltk.download("punkt")
+
+def speak(text):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    voice_id = 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Speech\\Voices\\Tokens\\TTS_MS_EN-US_DAVID_11.0'
+    engine.setProperty('voice', voice_id)
+    print("")
+    print(f"==> Revenant Ai : {text}")
+    print("")
+    engine.say(text)
+    engine.runAndWait()
+
+def speechrecognition():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening.....")
+        r.pause_threshold = 1
+        audio = r.listen(source, 0, 8)
+
+        try:
+            print("Recognizing....")
+            query = r.recognize_google(audio, language="en")
+            return query.lower()
+
+        except:
+            return ""
+
 
 ScriptDir = pathlib.Path().absolute()
 
@@ -13,20 +50,47 @@ chrome_option = Options()
 user_agent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2"
 chrome_option.add_argument(f"user-agent={user_agent}")
 chrome_option.add_argument('--profile-directory=Default')
-#chrome_option.add_argument("--headless=new")
+chrome_option.add_argument("--headless=new")
 chrome_option.add_argument(f'user-data-dir={ScriptDir}\\chromedata')
 service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=chrome_option)
+driver = webdriver.Chrome(service=service,options=chrome_option)
 
 driver.maximize_window()
 driver.get(url=url)
-sleep(100)
+sleep(5)
 
 def QuerySender(Query):
-    XpathInput = '/html/body/div/main/div/div/div[3]/div[1]/div[3]/div[2]/div/div[2]/textarea'
-    XpathSenderButton = '/html/body/div/main/div/div/div[3]/div[1]/div[3]/div[2]/div/button'
+    XpathInput = '/html/body/div/main/div/div/div[3]/div[1]/div[4]/div/div[2]/textarea'
+    XpathSenderButton = '/html/body/div/main/div/div/div[3]/div[1]/div[4]/div/button'
 
     driver.find_element(by=By.XPATH,value=XpathInput).send_keys(Query)
     sleep(1)
     driver.find_element(by=By.XPATH,value=XpathSenderButton).click()
     sleep(1)
+
+
+def wait_for_result():
+    sleep(1)
+    xpath_input = '/html/body/div/main/div/div/div[3]/div[1]/div[4]/div/div[2]/textarea'
+    driver.find_element(by=By.XPATH, value=xpath_input).send_keys("Testing...")
+    sleep(1)
+    while True:
+        Button = driver.find_element(by=By.XPATH, value="/html/body/div/main/div/div/div[3]/div[1]/div[4]/div/button").is_enabled()
+        try:
+            if False==Button:
+                pass
+            else:
+                driver.find_element(by=By.XPATH, value=xpath_input).clear()
+                break
+        except:
+            pass
+
+def Result():
+    Text = driver.find_element(by=By.XPATH, value="/html/body/div/main/div/div/div[3]/div[1]/div[2]/div/div/div/div[3]/div/div/div[2]").text
+    return Text
+
+while True:
+    Query = speechrecognition()
+    QuerySender(Query=Query)
+    wait_for_result()
+    speak(Result())
